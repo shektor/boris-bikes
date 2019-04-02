c5ada90ad81bc64f7829bf9d34492cc474f19c11
shektor <44544977+shektor@users.noreply.github.com>
2019-04-02 15:46:53 +0000

    Add initial release_bike unit test

--- spec/dockingstation_spec.rb
+++ spec/dockingstation_spec.rb
@@ -1,6 +1,10 @@
 require 'dockingstation'
 
 describe DockingStation do
-
+  describe '#release_bike' do
+    it 'should respond to release bike' do
+      expect(subject).to respond_to(:release_bike)
+    end
+  end
 
 end
