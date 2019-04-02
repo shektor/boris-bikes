7bc7d25a1ce43474534a3cfdd287cb3050e44063
Chenbo Yi <yichenbo@outlook.com>
2019-04-02 18:28:58 +0100

    Update the dockingstation_spec.rb to test return a bike and bike working?

--- spec/dockingstation_spec.rb
+++ spec/dockingstation_spec.rb
@@ -3,6 +3,7 @@ require 'dockingstation'
 describe DockingStation do
   describe '#release_bike' do
     it { is_expected.to respond_to(:release_bike) }
+    it { expect(subject.release_bike).to be_a Bike }
+    it { expect(subject.release_bike).to be_working }
   end
-
 end
