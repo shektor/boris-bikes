13f246b86c1fd775e188b7372b8b3601b725044d
shektor <44544977+shektor@users.noreply.github.com>
2019-04-02 15:52:20 +0000

    Refactored release_bike unit test to use 1 line

--- spec/dockingstation_spec.rb
+++ spec/dockingstation_spec.rb
@@ -2,9 +2,7 @@ require 'dockingstation'
 
 describe DockingStation do
   describe '#release_bike' do
-    it 'should respond to release bike' do
-      expect(subject).to respond_to(:release_bike)
-    end
+    it { is_expected.to respond_to(:release_bike) }
   end
 
 end
