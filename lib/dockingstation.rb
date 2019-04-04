81526857dadbbf907f403557a68f7d192c43694e
shektor <44544977+shektor@users.noreply.github.com>
2019-04-04 18:59:08 +0100

    Remove unused class and clean presentation

--- lib/dockingstation.rb
+++ lib/dockingstation.rb
@@ -1,15 +1,13 @@
 require_relative 'bike'
 
 class DockingStation
+  attr_reader :bike
+
   def release_bike
     Bike.new
   end
-attr_reader :bike
+
   def dock(bike)
     @bike = bike
-end
-
-def see
-end
-
+  end
 end
